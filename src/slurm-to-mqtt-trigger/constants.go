3adaa4e4d54a527d9e14ca6d14f81604fce3efeb
Andreas Maus <maus@badphish.ypbind.de>
2021-04-27 19:32:31 +0200

    Fix help text

--- src/slurm-to-mqtt-trigger/constants.go
+++ src/slurm-to-mqtt-trigger/constants.go
@@ -27,14 +27,14 @@ const helpText = `Usage: %s [--help] [--version] --mode=job|node --down|--draine
 
     --version       Show version information
 
-  Options for job mode, only one option can be used:
+  Options for node mode, only one option can be used:
     --down      Node entered DOWN state
     --drained   Node entered DRAINED state
     --fail      Node entered FAIL state
     --idle      Node was idle for a specified amount of time
     --up        Node returned from DOWN state
 
-  Options for node mode, only one option can be used:
+  Options for job mode, only one option can be used:
     --fini      Job finished
     --time      Job time limit reached
 
